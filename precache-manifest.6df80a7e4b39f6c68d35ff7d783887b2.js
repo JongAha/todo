b8cf031b691f11f6ff6bf5b2b715ec5365c2babe
JongAh <ljsxgz@163.com>
2024-10-22 12:39:50 +0800

    init

---
+++ precache-manifest.6df80a7e4b39f6c68d35ff7d783887b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46d438eff02aef48d2ed",
+    "url": "static/css/app.f2b96634.css"
+  },
+  {
+    "revision": "46d438eff02aef48d2ed",
+    "url": "static/js/app.5ae86c3d.js"
+  },
+  {
+    "revision": "aeb3fc11377d086a90fc",
+    "url": "static/css/chunk-vendors.834ea664.css"
+  },
+  {
+    "revision": "aeb3fc11377d086a90fc",
+    "url": "static/js/chunk-vendors.f409e341.js"
+  },
+  {
+    "revision": "36e2e578fe9b06995b88c69339f15341",
+    "url": "index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+];
